f4af1a55fabb60986f1b8a460d82fc4b2171e189
Laurent Claessens <moky.math@gmail.com>
2015-02-15 22:30:18 +0100

    Comment est-ce possible que ce bug ne se manifeste que maintenant ???

--- BasicGeometricObjects.py
+++ BasicGeometricObjects.py
@@ -508,9 +508,11 @@ def visual_length(v,l,xunit=None,yunit=None,pspict=None):
     .. image:: Picture_FIGLabelFigtestVisualLengthPICTtestVisualLength-for_eps.png
 
     """
+    print("XHOEooUPLjyV  début de visual_length")
     if pspict:
         xunit=pspict.xunit
         yunit=pspict.yunit
+    print('SMDRooUecrqo',xunit,yunit)
     Dx=v.Dx
     Dy=v.Dy
     if not v.vertical :
@@ -520,8 +522,11 @@ def visual_length(v,l,xunit=None,yunit=None,pspict=None):
             x=-x
         y=slope*x
     else:
+        print("GDMVooTzAclw ce vecteur est vertical")
         x=0
         y=l/yunit
+        if Dy<0:
+            y=-l/yunit
     if hasattr(v,"I"):
         from phystricks import AffineVector
         from phystricks import Vector
@@ -539,8 +544,11 @@ def visual_polar(P,r,theta,pspict=None):
     xunit=pspict.xunit
     yunit=pspict.yunit
     alpha=pi*theta/180
+    print("BOWUooEEJyPD alpha :",alpha)
     v=Vector( cos(alpha)/xunit,sin(alpha)/yunit  )
+    print("OMKZooUiPOFo v",v.I.coordinates(),v.F.coordinates())
     w=visual_length(v,r,pspict=pspict)
+    print("TQOKooEQvuRE w",w.I.coordinates(),w.F.coordinates())
     return P+w
 
 def visual_polar_coordinates(P,pspict=None):
@@ -675,8 +683,10 @@ class GraphOfASingleAxe(GraphOfAnObject):
             #a=circle.get_point(bar_angle,numerical=True)
             #b=circle.get_point(bar_angle+180,numerical=True)
 
+            print('CVHYooMJLRqI',bar_angle)
             a=visual_polar(P,0.1,bar_angle,pspict)
             b=visual_polar(P,0.1,bar_angle+180,pspict)
+            print("QVBVooEEHCnX",P.coordinates(),a.coordinates(),b.coordinates())
 
             seg=Segment(a,b)
             bars_list.append(seg)
