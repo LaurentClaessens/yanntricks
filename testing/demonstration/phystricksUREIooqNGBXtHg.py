4c2d59dae33dc17b6f6d1cb30cb72537b0093b18
Laurent Claessens <moky.math@gmail.com>
2016-03-04 11:57:29 +0100

    A test picture.

---
+++ testing/demonstration/phystricksUREIooqNGBXtHg.py
@@ -0,0 +1,35 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def UREIooqNGBXtHg():
+    pspict,fig = SinglePicture("UREIooqNGBXtHg")
+    pspict.dilatation(0.8)
+
+    A=Point(1,2)
+    B=Point(-1,2)
+    C=Point(3,0)
+    O=Point(0,0)
+
+    A.put_mark(0.2,angle=None,text="\( A\)",automatic_place=(pspict,""))
+    B.put_mark(0.2,angle=None,text="\( B\)",automatic_place=(pspict,""))
+    C.put_mark(0.2,angle=-90,text="\( C\)",automatic_place=(pspict,""))
+    O.put_mark(0.2,angle=225,text="\( O\)",automatic_place=(pspict,""))
+
+    pspict.math_BB.append( Point(4,4),pspict=pspict )
+
+    pspict.DrawGraphs(A,B,C,O)
+    pspict.DrawDefaultGrid()
+    pspict.comment=r"""
+    \begin{enumerate}
+    \item
+    The mark of the point \( A\)  is on the line \( OA\),
+    \item
+    the mark on $B$ on the line $OB$, 
+    \item
+    the mark on $C$ is at angle $-90$ degree
+    \item
+    and the one of \( O\) is at angle \( 225\) degree
+    \end{enumerate}
+    """
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
