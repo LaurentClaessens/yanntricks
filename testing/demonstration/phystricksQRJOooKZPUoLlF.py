ab077f4f51e0cdae109e28725d807efe634e6392
Laurent Claessens <moky.math@gmail.com>
2016-03-10 05:52:06 +0100

    A new testing picture

---
+++ testing/demonstration/phystricksQRJOooKZPUoLlF.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def QRJOooKZPUoLlF():
+    """
+    Copied from  LLVMooWOkvAB in mazhe
+    """
+    pspict,fig = SinglePicture("QRJOooKZPUoLlF")
+    x=var('x')
+    f1=phyFunction(x)
+    f2=phyFunction(x**2)
+
+    surface=SurfaceBetweenFunctions(f1,f2,mx=0,Mx=1)
+    surface.parameters.hatched()
+    surface.parameters.color="green"
+    surface.curve2.put_arrow(0.5)
+    surface.curve1.put_arrow(0.5)
+    surface.curve1.record_arrows[0]=-surface.curve1.record_arrows[0]
+
+    pspict.DrawGraphs(surface)
+    pspict.DrawDefaultAxes()
+    pspict.dilatation(3)
+    pspict.comment="lines in green, one arrow in each sense and area hatched in grey."
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
