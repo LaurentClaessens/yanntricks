4aee02cfc5f8bc5629d6df58cfd5618f8440149e
Laurent Claessens <moky.math@gmail.com>
2016-03-10 06:03:28 +0100

    Update a testing picture to illustrate the arc length functionality

--- testing/demonstration/FunctionFive.py
+++ testing/demonstration/FunctionFive.py
@@ -3,20 +3,26 @@ def FunctionFive():
     pspict,fig = SinglePicture("FunctionFive")
 
     var('x')
-    f = phyFunction( x*sin(x) )
     mx = -5
     Mx = 5
-    F=f.graph(mx,Mx)
+    f = phyFunction( x*sin(x) ).graph(mx,Mx)
 
     points = f.get_regular_points(mx,Mx,1.5)
 
-    pspict.DrawGraph(F)
+    pspict.DrawGraph(f)
     for i in range(0,len(points)):
         P = points[i]
-        P.put_mark(0.3,90,"$P_{%s}$"%str(i),automatic_place=(pspict,""))
+        P.put_mark(0.2,None,"$P_{%s}$"%str(i),automatic_place=(pspict,""))
         pspict.DrawGraph(P)
 
     pspict.DrawDefaultAxes()
+    pspict.comment=r"""
+    \begin{itemize}
+    \item 
+            Points are regularly spaced with respect to the arc length
+            \item 
+            Marks are on the exterior normal.
+    \end{itemize}"""
 
     fig.no_figure()
     fig.conclude()
--- testing/demonstration/FunctionFour.py
+++ testing/demonstration/FunctionFour.py
@@ -3,20 +3,20 @@ def FunctionFour():
     pspict,fig = SinglePicture("FunctionFour")
 
     var('x')
-    f = phyFunction( x*sin(x) )
     mx = -5
     Mx = 5
-    F=f.graph(mx,Mx)
+    f = phyFunction( x*sin(x) ).graph(mx,Mx)
     points = []
     for i in range(mx,Mx) :
         points.append(f.get_point(i))
 
-    pspict.DrawGraph(F)
+    pspict.DrawGraph(f)
     for i in range(0,len(points)):
-        points[i].put_mark(0.3,90,"$P_{%s}$"%str(i),automatic_place=(pspict,""))
+        points[i].put_mark(0.2,None,"$P_{%s}$"%str(i),automatic_place=(pspict,""))
         pspict.DrawGraph(points[i])
 
     pspict.DrawDefaultAxes()
+    pspict.comment="Points are regularly spaced with respect to abscisses."
 
     fig.no_figure()
     fig.conclude()
