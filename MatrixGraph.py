3ffbf03477a860f199bb57c538172d9113e3eabb
Laurent Claessens <moky.math@gmail.com>
2016-05-17 07:08:20 +0200

    Building of matrix seem to work.

--- MatrixGraph.py
+++ MatrixGraph.py
@@ -34,9 +34,16 @@ class MatrixElement(object):
     - first box : slightly larger box than the text box. If you want to draw a square around a part of the matrix, you should use this one
     - second box : slightly larger that the first one. The limit of the second box of one element is the same as the limit of the second box of the next element.
     """
-    def __init__(self,text):
+    def __init__(self,text,line,column,matrix):
         self.text=text
-        self.central_point=Point(0,0)
+        self.line=line
+        self.column=column
+        self.matrix=matrix
+        self.central_point=None
+    def getColumn(self):
+        return self.matrix.getColumn(self.column)
+    def getLine(self):
+        return self.matrix.getLine(self.line)
     def getTextSize(self,pspict):
         return pspict.get_box_size(self.text)
     def getTextHeight(self,pspict):
@@ -47,25 +54,46 @@ class MatrixElement(object):
         text_width,text_height=self.getTextSize(pspict)
         xmin=self.central_point.x-text_width/2
         xmax=self.central_point.x+text_width/2
-        ymin=self.central_point.y-text_width/2
-        ymax=self.central_point.y+text_width/2
+        ymin=self.central_point.y-text_height/2
+        ymax=self.central_point.y+text_height/2
         return BoundingBox(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax)
     def getFirstBox(self,pspict):
         text_box=self.getTextBox(pspict)
-        xmin=text_box.xmin-Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER
-        xmax=text_box.xmax+Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER
-        ymin=text_box.ymin-Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER
-        ymax=text_box.ymax+Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER
+        xmin=text_box.xmin-Defaults.MATRIX_ELEMENT_FIRST_BOX_X_BORDER
+        xmax=text_box.xmax+Defaults.MATRIX_ELEMENT_FIRST_BOX_X_BORDER
+        ymin=text_box.ymin-Defaults.MATRIX_ELEMENT_FIRST_BOX_Y_BORDER
+        ymax=text_box.ymax+Defaults.MATRIX_ELEMENT_FIRST_BOX_Y_BORDER
         return BoundingBox(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax)
     def getSecondBox(self,pspict):
         import Defaults
         first_box=self.getFirstBox(pspict)
-        xmin=text_box.xmin-Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER
-        xmax=text_box.xmax+Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER
-        ymin=text_box.ymin-Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER
-        ymax=text_box.ymax+Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER
+        xmin=text_box.xmin-Defaults.MATRIX_ELEMENT_FIRST_BOX_X_BORDER
+        xmax=text_box.xmax+Defaults.MATRIX_ELEMENT_FIRST_BOX_X_BORDER
+        ymin=text_box.ymin-Defaults.MATRIX_ELEMENT_FIRST_BOX_Y_BORDER
+        ymax=text_box.ymax+Defaults.MATRIX_ELEMENT_FIRST_BOX_Y_BORDER
         return BoundingBox(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax)
 
+class MatrixLineColumn(object):
+    def __init__(self,number,matrix):
+        self.number=number
+        self.matrix=matrix
+        self._height=None
+        self._width=None
+        self.elements=None
+    def getHeight(self,pspict):
+        import Defaults
+        if self._height==None:
+            h=max( [  el.getTextHeight(pspict) for el in self ])
+            self._height=h+2*(Defaults.MATRIX_ELEMENT_FIRST_BOX_Y_BORDER+Defaults.MATRIX_ELEMENT_SECOND_BOX_Y_BORDER)
+        return self._height
+    def getWidth(self,pspict):
+        import Defaults
+        if self._width==None:
+            h=max( [  el.getTextWidth(pspict) for el in self ])
+            self._width=h+2*(Defaults.MATRIX_ELEMENT_FIRST_BOX_X_BORDER+Defaults.MATRIX_ELEMENT_SECOND_BOX_X_BORDER)
+        return self._width
+    def __iter__(self):
+        return self.elements.__iter__()
 
 class MatrixGraph(ObjectGraph):
     """
@@ -76,35 +104,64 @@ class MatrixGraph(ObjectGraph):
         self.nlines=nlines
         self.ncolumns=ncolumns
         self.elements={}
-        self.column_width={}
-        self.line_heigth={}
-        for i in range(1,self.nlines+1):
-            self.line_heigth[i]=None
-        for i in range(1,self.ncolumns+1):
-            self.column_width[i]=None
+        self.lines={}
+        self.columns={}
         for i in range(1,self.nlines+1):
             for j in range (1,self.ncolumns+1):
-                self.elements[i,j]=MatrixElement("$({},{})$".format(i,j))
-
+                self.elements[i,j]=MatrixElement("$({},{})$".format(i,j),line=i,column=j,matrix=self)
+        for i in range(1,self.nlines+1):
+            line=MatrixLineColumn(i,self)
+            line.elements=[  self.elements[i,j] for j in range(1,self.ncolumns+1)  ]
+            self.lines[i]=line
+        for j in range(1,self.ncolumns+1):
+            col=MatrixLineColumn(j,self)
+            col.elements=[  self.elements[i,j] for i in range(1,self.nlines+1)  ]
+            self.columns[j]=col
+    def getMidPoint(self,pspict):
+        bb=self.elements[1,1].getTextBox(pspict)
+        xmin=self.elements[1,1].getTextBox(pspict).xmin
+        xmax=self.elements[1,self.ncolumns].getTextBox(pspict).xmax
+        ymin=self.elements[1,1].getTextBox(pspict).ymin
+        ymax=self.elements[self.nlines,self.ncolumns].getTextBox(pspict).ymax
+        return Point((xmin+xmax)/2,(ymin+ymax)/2)
     def getLine(self,n):
-        """ return the list of matrix elements on line number 'n' """
-        return [  self.elements[n,j] for j in range(1,self.nlines+1)  ]
+        return self.lines[n]
     def getColumn(self,m):
-        """ return the list of matrix elements on line number 'n' """
-        return [  self.elements[i,m] for i in range(1,self.ncolumns+1)  ]
-    def getLineHeigth(self,n,pspict):
-        import Defaults
-        if self.line_heigth[n] == None:
-            h=max( [  el.getTextHeight(pspict) for el in self.getLine(n)  ])
-            self.line_heigth[n]=h+2*(Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER+Defaults.MATRIX_ELEMENT_SECOND_BOX_BORDER)
-        return self.line_heigth[n]
-    def getColumnWidth(self,m,pspict):
-        import Defaults
-        if self.column_width[m] == None:
-            h=max( [  el.getTextWidth(pspict) for el in self.getColumn(m)  ])
-            self.column_width[m]=h+2*(Defaults.MATRIX_ELEMENT_FIRST_BOX_BORDER+Defaults.MATRIX_ELEMENT_SECOND_BOX_BORDER)
-        return self.column_width[m]
+        return self.columns[m]
+    def computeCentralPoints(self,pspict):
+        y=0
+        for i in range(1,self.nlines+1):
+            y=y-self.getLine(i).getHeight(pspict)/2
+            x=0
+            for j in range (1,self.ncolumns+1):
+                x=x+self.getColumn(j).getWidth(pspict)/2
+                self.elements[i,j].central_point=Point(x,y)
+                x=x+self.getColumn(j).getWidth(pspict)/2
+            y=y-self.getLine(i).getHeight(pspict)/2
+
 
     def action_on_pspict(self,pspict):
-        P=Point(0,0)
+        self.computeCentralPoints(pspict)
+        for i in range(1,self.nlines+1):
+            for j in range (1,self.ncolumns+1):
+                P=self.elements[i,j].central_point
+                P.put_mark(0,angle=0,text=self.elements[i,j].text,automatic_place=(pspict,"center"))
+                P.parameters.symbol=""
+                pspict.DrawGraphs(P)
+
+        l=[]
+        for i in range(1,self.nlines+1):
+            l.append("&".join( [  "\\text{"+el.text+"}" for el in self.getLine(i)  ]  ))
+        matrix_code="\\\\".join(l)
+        P=self.getMidPoint(pspict)
+        fake_matrix=r"""$
+        \begin{pmatrix}
+        \phantom{
+        \begin{matrix}
+        MATRIX_CODE
+        \end{matrix}
+        }
+        \end{pmatrix}$
+        """.replace("MATRIX_CODE",matrix_code)
+        P.put_mark(0,angle=0,text=fake_matrix,automatic_place=(pspict,"center"))
         pspict.DrawGraphs(P)
--- testing/demonstration/phystricksLWVXooPyIlOKNd.py
+++ testing/demonstration/phystricksLWVXooPyIlOKNd.py
@@ -5,19 +5,27 @@ def LWVXooPyIlOKNd():
     pspict.dilatation(1)
 
     matrix=phyMatrix(3,3)
-    matrix.elements[1,1].text="A"
-    matrix.elements[1,2].text="B"
-    matrix.elements[1,3].text="C"
+    matrix.elements[1,1].text="$\lim_{x\\to \infty}f(x)$"
+    matrix.elements[1,2].text="$B$"
+    matrix.elements[1,3].text="$C$"
     #matrix.elements[2,1].text=""
     matrix.elements[2,2].text="\( \int_a^b \)"
-    matrix.elements[2,3].text="\sin(x)"
+    matrix.elements[2,3].text="$\sin(x)$"
     #matrix.elements[3,1].text=""
-    matrix.elements[3,2].text="\Delta_k(A_m)"
-    matrix.elements[3,3].text="\\vdots"
-
-    print(matrix.getColumnWidth(1,pspict))
+    matrix.elements[3,2].text="$\Delta_k(A_m)$"
+    matrix.elements[3,3].text="$\\vdots$"
 
     pspict.DrawGraphs(matrix)
+    pspict.comment=r"""La matrice\\
+\begin{equation}
+    \begin{pmatrix}
+        \lim_{x\to \infty}f(x)    &   B    &   C    \\
+        (2,1)    &   \int_a^b    &   \sin(x)    \\
+        (3,1)    &   \Delta_k(A_m)    &   \vdots
+    \end{pmatrix}
+\end{equation}
+"""
+
     fig.no_figure()
     fig.conclude()
     fig.write_the_file()
