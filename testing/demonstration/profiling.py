86dbcbcae7e1616ffa78b7cbc61072656f512540
Laurent Claessens <moky.math@gmail.com>
2016-08-11 11:00:04 +0200

    A test file for the mark on angle.

---
+++ testing/demonstration/profiling.py
@@ -0,0 +1,9 @@
+#! /usr/bin/sage -python
+# -*- coding: utf8 -*-
+
+import cProfile
+from phystricks import *
+
+from phystricksCUZFooGqZLaAEp import CUZFooGqZLaAEp
+
+cProfile.run("CUZFooGqZLaAEp()")
