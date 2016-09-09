e5fbe0308ba310f5b7edf09d981a7da1ea68542c
Laurent Claessens <moky.math@gmail.com>
2016-09-09 05:38:30 +0200

    Some testing setting.

---
+++ testing/testing_pictures/lst_actu.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+from __future__ import unicode_literals
+
+import LaTeXparser
+import LaTeXparser.PytexTools
+import plugins_agreg
+
+myRequest = LaTeXparser.PytexTools.Request("mesure")
+
+myRequest.add_plugin(plugins_agreg.set_isAgreg,"before_pytex")
+
+myRequest.original_filename="testing.tex"
+
+myRequest.ok_filenames_list=["e_testing"]
+myRequest.ok_filenames_list.extend(["1_testing"])
+
+
+myRequest.new_output_filename="0-actu.pdf"
