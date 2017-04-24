b689a28bc0c6f1fd21fc3803c5b5a86c3afbba35
Laurent Claessens <laurent@claessens-donadello.eu>
2017-04-24 21:41:44 +0200

    Three times compiling the demonstration file.

--- testing/testing.sh
+++ testing/testing.sh
@@ -10,13 +10,34 @@ pytex lst_demo.py --no-external --all
 }
 
 
-cd unit_tests
-./unit_tests.sh
-cd ../demonstration
+################" DEMONSTRATION FILE ############
 
-# Compile twice the demo pictures
+cd demonstration
+
+# Remove the garbage files
+rm *.dpth
+rm *.log
+rm *.md5
+rm *.pdf
+rm *.pstricks
+rm *.comment
+rm *.aux
+rm *.pyc
+
+# Compile three times the demo pictures 
+# (yes, some pictures need three passes)
+
+compile_demo
 compile_demo
 compile_demo
 
 # Then compare with the "recall" ones
 ./test_recall.py
+
+############### UNIT TESTING ###############
+
+cd ..
+
+cd unit_tests
+./unit_tests.sh
+
