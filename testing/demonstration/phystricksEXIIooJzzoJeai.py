6671775c2a4fa192b3a36e3fa8d541e75d85d77e
Laurent Claessens <laurent@claessens-donadello.eu>
2017-05-03 20:32:02 +0200

    Remove the numbering on the axes in 'EXIIooJzzoJeai' Thus it should work with only one pass. Since this is by far the most long picture to be compiled, I hope to somewhat shorten the tests duration.

--- testing/demonstration/phystricksEXIIooJzzoJeai.py
+++ testing/demonstration/phystricksEXIIooJzzoJeai.py
@@ -16,10 +16,10 @@ def EXIIooJzzoJeai():
 
     xmin=0.05
     x=var('x')
-    f1=phyFunction( sin(1/x)  ).graph(xmin,6)
-    f2=phyFunction( sin(1/x)  ).graph(xmin,6)
-    f3=phyFunction( sin(1/x)  ).graph(xmin,6)
-    f4=phyFunction( sin(1/x)  ).graph(xmin,6)
+    f1=phyFunction( sin(1/x) ).graph(xmin,6)
+    f2=phyFunction( sin(1/x) ).graph(xmin,6)
+    f3=phyFunction( sin(1/x) ).graph(xmin,6)
+    f4=phyFunction( sin(1/x) ).graph(xmin,6)
 
     f2.added_plotpoints=[2/(k*pi) for k in range(1,13)]  
     f3.linear_plotpoints=5000
@@ -31,6 +31,7 @@ def EXIIooJzzoJeai():
     pspicts[3].DrawGraphs(f4)
 
     for psp in pspicts:
+        psp.axes.no_numbering()
         psp.DrawDefaultAxes()
 
     fig.conclude()
