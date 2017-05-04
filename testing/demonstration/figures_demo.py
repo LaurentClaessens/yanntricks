17ed586996983849c490d79d013745d11474afb1
Laurent Claessens <laurent@claessens-donadello.eu>
2017-05-04 10:54:36 +0200

    Remove the print of the length of the pictures list.

--- testing/demonstration/figures_demo.py
+++ testing/demonstration/figures_demo.py
@@ -173,10 +173,6 @@ figures_list_3=[]
 figures_list_3.append(AESIooxXxRYQdT)
 figures_list_3.append(FunctionFour)
 
-print(len(figures_list_1))
-print(len(figures_list_2))
-print(len(figures_list_3))
-
 def AllFigures():
 
     figures_list=figures_list_1
