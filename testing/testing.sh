a6264063faf69c332cae3cbfd53bc37e43fd13c8
Laurent Claessens <laurent@claessens-donadello.eu>
2017-06-17 07:12:19 +0200

    (unit test) Bug fix. Replace BASEDIR -> BASE_DIR

--- testing/testing.sh
+++ testing/testing.sh
@@ -7,14 +7,14 @@
 # We append this version of 'phystricks' to $SAGE_PATH because 
 # we want to test what is here.
 
-BASEDIR=$(pwd)
-SAGE_PATH=$PYTHONPATH:$BASEDIR/../..
-RECALLTEST_DIR=$BASEDIR/recall_tests/
+BASE_DIR=$(pwd)
+SAGE_PATH=$PYTHONPATH:$BASE_DIR/../..
+RECALLTEST_DIR=$BASE_DIR/recall_tests/
 DEMO_DIR=$BASE_DIR/demonstration
-MANUAL_DIR=$BASE_DIR/../manual
-UNIT_TESTS_DIR=$BASEDIR/unit_tests
+MANUAL_DIR=$BASE_DIR/../manual/
+UNIT_TESTS_DIR=$BASE_DIR/unit_tests
 
-LOGFILE=$BASEDIR/testing.log
+LOGFILE=$BASE_DIR/testing.log
 rm $LOGFILE
 touch $LOGFILE
 
@@ -39,7 +39,6 @@ unit_testing ()
     cd $UNIT_TESTS_DIR
     SAGE_PATH=$SAGE_PATH ./testing.sh
 }
-
 
 unit_testing &&
 manual_testing&&
