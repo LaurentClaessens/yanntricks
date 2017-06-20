a2ebcc67fca3c424e317cc417f214d99aa481e2d
Laurent Claessens <laurent@claessens-donadello.eu>
2017-06-20 11:02:36 +0200

    (test) Remove some lines 'figures_list=[]' that were there for testing purposes.

--- testing/demonstration/figures_demo.py
+++ testing/demonstration/figures_demo.py
@@ -118,12 +118,6 @@ append_picture(SMXRooCnrlNw,1)
 append_picture(TKXZooLwXzjS,1)
 append_picture(exCircle,1)
 append_picture(OnePoint,1)
-
-
-figures_list_1=[]
-figures_list_2=[]
-figures_list_3=[]
-
 append_picture(FunctionSecond,1)
 append_picture(GridThree,1)
 append_picture(GridTwo,1)
