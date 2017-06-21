(bug fix) Remove the argument 'numerical' to some invocations of 'PointGraph.coordinates'

src/AffineVectorGraph.py
@@ -230,8 +230,8 @@ class AffineVectorGraph(ObjectGraph):
     def tikz_code(self,pspict=None):
         params=self.params(language="tikz")
         params=params+",->,>=latex"
-        I_coord = self.I.coordinates(numerical=True,digits=5,pspict=pspict)
-        F_coord = self.F.coordinates(numerical=True,digits=5,pspict=pspict)
+        I_coord = self.I.coordinates(digits=5,pspict=pspict)
+        F_coord = self.F.coordinates(digits=5,pspict=pspict)
         a = "\draw [{0}] {1} -- {2};".format(params,I_coord,F_coord,pspict=pspict)
         return a
     def latex_code(self,language=None,pspict=None):

src/SegmentGraph.py
@@ -960,8 +960,8 @@ class SegmentGraph(ObjectGraph):
         else:
             if language=="tikz":
                 a=[]
-                c1=self.I.coordinates(numerical=True,digits=3,pspict=pspict)
-                c2=self.F.coordinates(numerical=True,digits=3,pspict=pspict)
+                c1=self.I.coordinates(digits=3,pspict=pspict)
+                c2=self.F.coordinates(digits=3,pspict=pspict)
                 if 'I' in c1 or "I" in c2 :
                     from Exception import ImaginaryPartException
                     raise ImaginaryPartException(